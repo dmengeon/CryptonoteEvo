ed585ae5576029ea5c2e8ef42511a774010baa05
root <root@nur1labs.nur1labs.net>
2018-03-04 22:32:29 +0900

    updates main src

--- src/CryptoNote.hpp
+++ src/CryptoNote.hpp
@@ -1,27 +1,13 @@
-// Copyright (c) 2012-2018, The CryptoNote developers, The Bytecoin developers, [ ] developers
-//
-// This file is part of Bytecoin.
-//
-// Bytecoin is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Bytecoin is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+// Copyright (c) 2012-2018, The CryptoNote developers, The Bytecoin developers, [ ] developers.
+// Licensed under the GNU Lesser General Public License. See LICENSING.md for details.
 
 #pragma once
 
 #include <boost/variant.hpp>
 #include <functional>
 #include <vector>
-#include "crypto/types.hpp"
 #include "common/BinaryArray.hpp"
+#include "crypto/types.hpp"
 
 // We define here, as CryptoNoteConfig.h is never included anywhere anymore
 #define ALLOW_DEBUG_COMMANDS 1
@@ -72,7 +58,7 @@ struct TransactionOutput {
 };
 
 struct TransactionPrefix {
-	uint8_t version = 0;
+	uint8_t version          = 0;
 	UnlockMoment unlock_time = 0;
 	std::vector<TransactionInput> inputs;
 	std::vector<TransactionOutput> outputs;
@@ -98,8 +84,8 @@ struct ParentBlock {
 struct BlockHeader {
 	uint8_t major_version = 0;
 	uint8_t minor_version = 0;
-	uint32_t nonce = 0;
-	Timestamp timestamp = 0;
+	uint32_t nonce        = 0;
+	Timestamp timestamp   = 0;
 	Hash previous_block_hash;
 };
 
