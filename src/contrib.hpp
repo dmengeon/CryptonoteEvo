231d3d3d8839c00e52fb9453246d8273b94af9fe
Shiro <otakugroup2015@gmail.com>
2018-04-09 18:47:11 +0900

    貢献するために著作権を移す
    貢献するために著作権を移す

--- src/contrib.hpp
+++ src/contrib.hpp
@@ -1,8 +1,25 @@
 // Copyright (c) 2012-2018
 /* 
+==================Core Developer==================
 The CryptoNote developers
 The Bytecoin developers
+==================Add On Developer================
 [ ] developers
+- Sora as Core Platform
+- Shiro as Compiler and Bug Solver
+- Homdx as Docker Engineer
+- James as GUI Specialist
+====================Algorithm=====================
+Brian Gladman, Worcester, UK.[aesb]
+http://131002.net/blake/[blake]
+HAMC[blake]
+D. J. Bernstein[chacha]
+Thomas Krinninger[groestl]
+Soeren S. Thomsen and Krystian Matusiewicz[groestl]
+Nabil S. Al Ramli, www.nalramli.com[openaes]
+Markku-Juhani O. Saarinen <mjos@iki.fi>[keccak]
+Doug Whiting, 2008[skein]
+=================Platform Developer===============
 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 
 
